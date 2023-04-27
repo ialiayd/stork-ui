3c766dcedd8fd2d19c6a075075457a0887401bc6
ialiayd <ali.aydemir93@hotmail.com>
2023-04-27 13:06:47 +0300

    fix : repl

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,25 +1,25 @@
 // ** Router imports
-import { lazy } from 'react'
+import { lazy } from "react"
 
 // ** Router imports
-import { useRoutes, Navigate } from 'react-router-dom'
+import { useRoutes, Navigate } from "react-router-dom"
 
 // ** Layouts
-import BlankLayout from '@layouts/BlankLayout'
+import BlankLayout from "@layouts/BlankLayout"
 
 // ** Hooks Imports
-import { useLayout } from '@hooks/useLayout'
+import { useLayout } from "@hooks/useLayout"
 
 // ** Utils
-import { getUserData, getHomeRouteForLoggedInUser } from '../utility/Utils'
+import { getUserData, getHomeRouteForLoggedInUser } from "../utility/Utils"
 
 // ** GetRoutes
-import { getRoutes } from './routes'
+import { getRoutes } from "./routes"
 
 // ** Components
-const Error = lazy(() => import('../views/pages/misc/Error'))
-const Login = lazy(() => import('../views/pages/authentication/Login'))
-const NotAuthorized = lazy(() => import('../views/pages/misc/NotAuthorized'))
+const Error = lazy(() => import("../views/pages/misc/Error"))
+const Login = lazy(() => import("../views/pages/authentication/Login"))
+const NotAuthorized = lazy(() => import("../views/pages/misc/NotAuthorized"))
 
 const Router = () => {
   // ** Hooks
@@ -31,32 +31,32 @@ const Router = () => {
     if (user) {
       return getHomeRouteForLoggedInUser(user.role)
     } else {
-      return '/login'
+      return "/login"
     }
   }
 
   const routes = useRoutes([
     {
-      path: '/',
+      path: "/",
       index: true,
-      element: <Navigate replace to={getHomeRoute()} />
+      element: <Navigate replace to={getHomeRoute()} />,
     },
     {
-      path: '/login',
+      path: "/login",
       element: <BlankLayout />,
-      children: [{ path: '/login', element: <Login /> }]
+      children: [{ path: "/login", element: <Login /> }],
     },
     {
-      path: '/auth/not-auth',
+      path: "/auth/not-auth",
       element: <BlankLayout />,
-      children: [{ path: '/auth/not-auth', element: <NotAuthorized /> }]
+      children: [{ path: "/auth/not-auth", element: <NotAuthorized /> }],
     },
     {
-      path: '*',
+      path: "*",
       element: <BlankLayout />,
-      children: [{ path: '*', element: <Error /> }]
+      children: [{ path: "*", element: <Error /> }],
     },
-    ...allRoutes
+    ...allRoutes,
   ])
 
   return routes
--- src/router/routes/index.js
+++ src/router/routes/index.js
@@ -1,43 +1,43 @@
 // ** React Imports
-import { Fragment } from 'react'
+import { Fragment } from "react"
 
 // ** Routes Imports
-import AppRoutes from './Apps'
-import FormRoutes from './Forms'
-import PagesRoutes from './Pages'
-import TablesRoutes from './Tables'
-import ChartsRoutes from './Charts'
-import DashboardRoutes from './Dashboards'
-import UiElementRoutes from './UiElements'
-import ExtensionsRoutes from './Extensions'
-import PageLayoutsRoutes from './PageLayouts'
-import AuthenticationRoutes from './Authentication'
-import StorkRoutes from './Stork'
+import AppRoutes from "./Apps"
+import FormRoutes from "./Forms"
+import PagesRoutes from "./Pages"
+import TablesRoutes from "./Tables"
+import ChartsRoutes from "./Charts"
+import DashboardRoutes from "./Dashboards"
+import UiElementRoutes from "./UiElements"
+import ExtensionsRoutes from "./Extensions"
+import PageLayoutsRoutes from "./PageLayouts"
+import AuthenticationRoutes from "./Authentication"
+import StorkRoutes from "./Stork"
 
 // ** Layouts
-import BlankLayout from '@layouts/BlankLayout'
-import VerticalLayout from '@src/layouts/VerticalLayout'
-import HorizontalLayout from '@src/layouts/HorizontalLayout'
-import LayoutWrapper from '@src/@core/layouts/components/layout-wrapper'
+import BlankLayout from "@layouts/BlankLayout"
+import VerticalLayout from "@src/layouts/VerticalLayout"
+import HorizontalLayout from "@src/layouts/HorizontalLayout"
+import LayoutWrapper from "@src/@core/layouts/components/layout-wrapper"
 
 // ** Route Components
-import PublicRoute from '@components/routes/PublicRoute'
-import PrivateRoute from '@components/routes/PrivateRoute'
+import PublicRoute from "@components/routes/PublicRoute"
+import PrivateRoute from "@components/routes/PrivateRoute"
 
 // ** Utils
-import { isObjEmpty } from '@utils'
+import { isObjEmpty } from "@utils"
 
 const getLayout = {
   blank: <BlankLayout />,
   vertical: <VerticalLayout />,
-  horizontal: <HorizontalLayout />
+  horizontal: <HorizontalLayout />,
 }
 
 // ** Document title
-const TemplateTitle = '%s - Vuexy React Admin Template'
+const TemplateTitle = "%s - Vuexy React Admin Template"
 
 // ** Default Route
-const DefaultRoute = '/dashboard/ecommerce'
+const DefaultRoute = "/home"
 
 // ** Merge Routes
 const Routes = [
@@ -51,10 +51,10 @@ const Routes = [
   ...FormRoutes,
   ...TablesRoutes,
   ...ChartsRoutes,
-  ...StorkRoutes
+  ...StorkRoutes,
 ]
 
-const getRouteMeta = route => {
+const getRouteMeta = (route) => {
   if (isObjEmpty(route.element.props)) {
     if (route.meta) {
       return { routeMeta: route.meta }
@@ -69,18 +69,19 @@ const MergeLayoutRoutes = (layout, defaultLayout) => {
   const LayoutRoutes = []
 
   if (Routes) {
-    Routes.filter(route => {
+    Routes.filter((route) => {
       let isBlank = false
       // ** Checks if Route layout or Default layout matches current layout
       if (
         (route.meta && route.meta.layout && route.meta.layout === layout) ||
-        ((route.meta === undefined || route.meta.layout === undefined) && defaultLayout === layout)
+        ((route.meta === undefined || route.meta.layout === undefined) &&
+          defaultLayout === layout)
       ) {
         let RouteTag = PrivateRoute
 
         // ** Check for public or private route
         if (route.meta) {
-          route.meta.layout === 'blank' ? (isBlank = true) : (isBlank = false)
+          route.meta.layout === "blank" ? (isBlank = true) : (isBlank = false)
           RouteTag = route.meta.publicRoute ? PublicRoute : PrivateRoute
         }
         if (route.element) {
@@ -107,19 +108,19 @@ const MergeLayoutRoutes = (layout, defaultLayout) => {
   return LayoutRoutes
 }
 
-const getRoutes = layout => {
-  const defaultLayout = layout || 'vertical'
-  const layouts = ['vertical', 'horizontal', 'blank']
+const getRoutes = (layout) => {
+  const defaultLayout = layout || "vertical"
+  const layouts = ["vertical", "horizontal", "blank"]
 
   const AllRoutes = []
 
-  layouts.forEach(layoutItem => {
+  layouts.forEach((layoutItem) => {
     const LayoutRoutes = MergeLayoutRoutes(layoutItem, defaultLayout)
 
     AllRoutes.push({
-      path: '/',
+      path: "/",
       element: getLayout[layoutItem] || getLayout[defaultLayout],
-      children: LayoutRoutes
+      children: LayoutRoutes,
     })
   })
   return AllRoutes
